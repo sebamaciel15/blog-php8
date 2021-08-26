933f212896015edd9c737653a78573ff9f128523
Sebastian Maciel <sebamaciel15@gmail.com>
2021-08-26 10:57:21 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
